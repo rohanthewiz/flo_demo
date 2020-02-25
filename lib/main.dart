eee3f69ca7375c08744b7573db728f0b6c8d8e51
rohanthewiz <rohanthewiz@gmail.com>
2020-02-25 01:32:58 -0600

    Get Nested Build working

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yaml/yaml.dart';
 
 void main() {
-  YamlMap doc = loadYaml(
-      """
+  YamlMap doc = loadYaml("""
 type: container # There can be only one root node
 child-align: center
 background-color: "#ffeef0"
@@ -13,57 +12,27 @@ children:
     font-size: 1.4
     font-weight: bold
     color: blue
-  - type: image
-    src: "https://www.ccswm.org/assets/img/IMG_0007.JPG.b9c099ce0f6d1493.JPG"
-    align: right
-  - text: "What a lovely image above!"
-    type: text # you don't need to specify a type for text, but is not illegal :-)
-    class: comment
-  - type: ordered-list
-    children:
-      - type: li
-        text: Option 1
-      - type: li
-        text: Option 2
-  - type: row
-    align: left
-    xalign: start # top
-    children:
-      - text: "First"
-      - text: "Last"
   - type: column
     align: top
     xalign: right
     children:
       - text: "First Row"
       - text: "Second Row"
-  - type: table
-    columns:
-      - text: "Heading 1"
-      - text: "Heading 2"
-      - text: "Heading 3"
-    rows:
-      - type: row
-        children:
-          - text: "Cell 1"
-          - text: "Cell 2"
-      - type: row
-        children:
-          - text: "Cell 3"
-          - text: "Cell 4"
+  - type: row
+    align: left
+    xalign: start # top
+    children:
+      - text: "First Cell  "
+      - text: "Last Cell"
+  - type: image
+    src: "https://cdn.pixabay.com/photo/2016/03/09/15/27/cat-1246659_960_720.jpg"
+    width: 240
+    height: 320
+    align: right
+  - text: "What a lovely image above!"
+    type: text # you don't need to specify a type for text, but is not illegal :-)
+    class: comment
 """);
-
-  doc.forEach((k,v) {
-    if (k == 'children') {
-      YamlList nodes = doc['children'];
-//      nodes.forEach((child) {
-//        child.forEach((k,v) => print('  $k -> $v'));
-//      });
-      //
-    } else {
-      print('$k -> $v');
-    }
-  });
 
   runApp(
       MaterialApp(
@@ -92,50 +61,96 @@ List<Widget> buildWidgets(YamlList nodes) {
     var textVal = "";
     var color = 0xff101010;
     var fontWeight = FontWeight.normal;
+    int padding;
+    int width, height;
     var src = "#";
+    String xAlign;
+    List<Widget> children;
+    Widget gchild;
 
     child.forEach((k, v) {
       if (k == 'children') {
-        // Need to tie this back to the parent widget
-        // Widget Row( children: buildWidgets(v))
+        // Recursively build children
+        children = buildWidgets(v);
       } else {
         print('  $k -> $v');
         if (k == "text") { // TODO - use a switch
           textVal = v;
+        } else if (k == 'type') {
+          type = v;
         } else if (k == "color") {
           color = 0xff2440a0; // TODO: lookup v in a map
         } else if (k == 'font-weight') {
           fontWeight = FontWeight.bold; // TODO: lookup v in a map
-        } else if (k == 'image') {
-          type = 'image';
+        } else if (k == 'src') {
           src = v;
+        } else if (k == 'padding') {
+          padding = v;
+        } else if (k == 'width') {
+          width = v;
+        } else if (k == 'height') {
+          height = v;
         }
       }
     });
 
+    // Build
+
     if (type == "text") {
-      w.add(RichText(text:
-      TextSpan(text: textVal,
-          style: TextStyle(color: Color(color),
-            fontWeight: fontWeight,
-          ))));
+      w.add(Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: RichText(text:
+        TextSpan(text: textVal,
+            style: TextStyle(color: Color(color),
+              fontWeight: fontWeight,
+            ))),
+      ));
     } else if (type == "image") {
-      w.add(Image(image: NetworkImage(src), alignment: Alignment.centerRight));
+      w.add(Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Image.network(src, width: null, height: height.ceilToDouble()),
+      ));
+    } else if (type == 'column') {
+      w.add(Column(children: children));
+    } else if (type == 'row') {
+      w.add(Row(children: children));
     }
   });
+
+  return w;
+}
+
+// SCRAPS
+
+//- type: ordered-list
+//children:
+//- type: li
+//text: Option 1
+//- type: li
+//text: Option 2
+//- type: table
+//column:
+//- text: "Heading 1"
+//- text: "Heading 2"
+//- text: "Heading 3"
+//rows:
+//- type: row
+//children:
+//- text: "Cell 1"
+//- text: "Cell 2"
+//- type: row
+//children:
+//- text: "Cell 3"
+//- text: "Cell 4"
 
 //  doc.forEach((k,v) {
 //    if (k == 'children') {
 //      YamlList nodes = doc['children'];
-////      nodes.forEach((child) {
-////        child.forEach((k,v) => print('  $k -> $v'));
-////      });
+//      nodes.forEach((child) {
+//        child.forEach((k,v) => print('  $k -> $v'));
+//      });
 //      //
 //    } else {
 //      print('$k -> $v');
 //    }
 //  });
-
-  w.add(Text("A Text Widget"));
-  return w;
-}
